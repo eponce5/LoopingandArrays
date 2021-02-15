90da1be969ce383f3c0bd5a2532f0408da864411
eponce5 <eponce5@mail.valenciacollege.edu>
2021-02-14 22:43:35 -0500

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,20 @@
+ 
+   {
+ var myArray = [];
+
+for (var i = 1000; i < 0; i += 2) {
+myArray.push(i);
+}       
+    }
+const favoritetvshows = [" On my block", " Stranger things", " Breaking Bad", " Greys Anatomy", " How I met your Mother"];
+
+console.log("My #1 favorite tv show is" + favoritetvshows[0] );
+console.log("My #2 favorite tv show is" + favoritetvshows[1]);
+console.log("My #3 favorite tv show is" + favoritetvshows[2]);
+console.log("My #4 favorite tv show is" + favoritetvshows[3]);
+console.log("My #5 favorite tv show is" + favoritetvshows[4]);
+            
+   {
+console.log(favoritetvshows);
+               
+   }
---
+++ loops.js
@@ -0,0 +1,33 @@
+{
+for (var i = 0; i <= 1000; i++){
+   console.log(i);
+ 
+}
+
+console.log("First 1000 to 0 loop finished");
+
+}
+
+
+    for (var i= 0; i <= 2500; i++) {
+    if (i % 4 === 0) {
+           alert("A quarter of the way there!");
+
+    }
+    
+    for (var i= 0; i <= 5000; i++) {
+  if (i % 5 === 0) {
+        alert("Halfway there!");
+  }
+  
+    for (var i= 0; i <= 10000; i++) {
+ if (i % 10000 === 0) {
+     alert("The loop is done!");
+     
+       console.log(i);
+       
+      }
+
+    }
+console.log("Loop 0 to 10000 finished"); }
+}
